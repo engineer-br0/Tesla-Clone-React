03dc9767e840b1984f645c0542b3223290baff81
engineer-br0 <106507655+engineer-br0@users.noreply.github.com>
2022-09-01 10:46:14 +0530

    for

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,28 +7,28 @@ function Home() {
 		<Container>
 			<Section 
 				title="Model S" 
-				description="Order Online fro Touchless Delivery"
+				description="Order Online for Touchless Delivery"
 				backgroundImg="model-s.jpg"
 				leftBtnText="Custom order"
 				rightBtnText="Exisiting inventory"
 			/>
 			<Section 
 				title="Model 3" 
-				description="Order Online fro Touchless Delivery"
+				description="Order Online for Touchless Delivery"
 				backgroundImg="model-3.jpg"
 				leftBtnText="Custom order"
 				rightBtnText="Exisiting inventory"
 			/>
 			<Section 
 				title="Model X" 
-				description="Order Online fro Touchless Delivery"
+				description="Order Online for Touchless Delivery"
 				backgroundImg="model-x.jpg"
 				leftBtnText="Custom order"
 				rightBtnText="Exisiting inventory"
 			/>
 			<Section 
 				title="Model Y" 
-				description="Order Online fro Touchless Delivery"
+				description="Order Online for Touchless Delivery"
 				backgroundImg="model-y.jpg"
 				leftBtnText="Custom order"
 				rightBtnText="Exisiting inventory"
